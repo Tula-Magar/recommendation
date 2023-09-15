fe2899e11ae0c4fdc130aa934429986bb346e65d
TulaMagar <tmagar448@gmail.com>
2023-09-15 12:03:12 -0400

    added previous and next movie button

--- src/MovieDetailPage.tsx
+++ src/MovieDetailPage.tsx
@@ -1,26 +1,58 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import MovieDetail from "./MovieDetails"; // Import your MovieDetail component
-import { movies } from "./movieData"; // Import your movieData
-import { Movie } from "./movieTypes"; // Import your Movie type
+import React, { useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import MovieDetail from "./MovieDetails";
+import { movies } from "./movieData";
+import { Movie } from "./movieTypes";
 
 function MovieDetailPage() {
   const { id } = useParams();
+  const navigate = useNavigate(); // Get the navigate function to handle navigation
 
-  // Find the movie with the given ID or set default values if not found
+  // Find the index of the current movie in the list
+  const currentIndex = movies.findIndex((movie) => movie.id === Number(id));
+
+  // Calculate the index of the next and previous movies
+  const nextIndex = (currentIndex + 1) % movies.length; // Wrap around to the first movie
+  const prevIndex = (currentIndex - 1 + movies.length) % movies.length; // Wrap around to the last movie
+
+  // Get the next and previous movies
+  const nextMovie = movies[nextIndex];
+  const prevMovie = movies[prevIndex];
+
   const movie = (movies.find((movie) => movie.id === Number(id)) || {
     id: 0,
     title: "Not Found",
     genre: "N/A",
     imagesAndText: [],
-    Page: "", // Set to an empty string if the movie doesn't have a page
+    Page: "",
   }) as Movie;
+
+  // Function to navigate to the previous movie
+  const goToPreviousMovie = () => {
+    navigate(`/movie/${prevMovie.id}`);
+  };
+
+  // Function to navigate to the next movie
+  const goToNextMovie = () => {
+    navigate(`/movie/${nextMovie.id}`);
+  };
 
   return (
     <div>
       <h1>Movie Detail Page</h1>
+      <div className="navigation-buttons">
+        <button onClick={() => window.history.back()}>Back</button>
+        <button onClick={() => window.history.forward()}>Forward</button>
+      </div>
+
+      {/* Style these as buttons */}
+      <button className="previous-movie" onClick={goToPreviousMovie}>
+        Previous
+      </button>
+
+      <button onClick={goToNextMovie}>Next</button>
+
       {movie.Page ? (
-        // Dynamically import and render the movie.Page component
         (() => {
           const MoviePageComponent = require(`./${movie.Page}`).default;
           return <MoviePageComponent />;
@@ -28,6 +60,13 @@ function MovieDetailPage() {
       ) : (
         <MovieDetail movie={movie} />
       )}
+
+      {/* Style these as buttons */}
+      <button className="previous-movie" onClick={goToPreviousMovie}>
+        Previous
+      </button>
+
+      <button onClick={goToNextMovie}>Next</button>
     </div>
   );
 }
